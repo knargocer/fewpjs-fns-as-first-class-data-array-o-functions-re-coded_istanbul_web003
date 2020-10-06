d42e01321495b2705d34e0aec460cc8f68ad9ec2
Knar Gocer <knargr@gmail.com>
2020-10-06 14:38:38 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+function wakeDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+function leashDog(dogName, dogBreed) {
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+}
+
+function walkToPark(dogName, dogBreed) {
+  console.log(`Walk to the park with  ${dogName} the ${dogBreed}`);
+}
+
+function throwFrisbee(dogName, dogBreed) {
+  console.log(`Throw the frisbee for  ${dogName} the ${dogBreed}`);
+}
+
+function walkHome(dogName, dogBreed) {
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+}
+
+function unleashDog(dogName, dogBreed) {
+  console.log(`Unleash  ${dogName} the ${dogBreed}`);
+}
+
+console.log(wakeDog("a","b"))
+
+
+const routine = [wakeDog(dogName,dogBreed), leashDog(dogName,dogBreed), walkToPark(dogName,dogBreed), throwFrisbee(dogName,dogBreed), walkHome(dogName,dogBreed), unleashDog(dogName,dogBreed)];
+
+function exerciseDog(dogName,dogBreed) {
+  const result = [];
+  for(let i = 0 ; i<routine.length ; i++){
+        result.push(routine[i]);
+  }
+  return results;
+}

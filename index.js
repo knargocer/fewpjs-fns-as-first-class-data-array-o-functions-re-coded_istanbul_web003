10925ca2dd3fa9483269c3396e27d7b912725c75
Knar Gocer <knargr@gmail.com>
2020-10-06 14:40:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ function wakeDog(dogName, dogBreed) {
 }
 function leashDog(dogName, dogBreed) {
   console.log(`Leash ${dogName} the ${dogBreed}`);
+  return `Leash ${dogName} the ${dogBreed`;
 }
 
 function walkToPark(dogName, dogBreed) {
   console.log(`Walk to the park with  ${dogName} the ${dogBreed}`);
+  return `Walk to the park with  ${dogName} the ${dogBreed}`;
 }
 
 function throwFrisbee(dogName, dogBreed) {
